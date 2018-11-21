00acfdf58a875b858d61f87b1500a42a3fedf80a
Claudia Fox <claudiafox95@icloud.com>
2018-11-21 21:37:45 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :artist, :genre, :artists, :genres
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@songs = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @@songs << name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count = @@count + 1
+   # binding.pry
+  end
+  
+  def self.artists
+    @@artists.uniq!
+  end
+  
+  def self.genres
+    @@genres.uniq!
+  end
+  
+  def self.count
+    @@count
+  end
+
+   def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
+  
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] +=1; total}
+  end
+end
